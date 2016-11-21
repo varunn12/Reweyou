83168a66f13bfea71dbef0ce4b227d6b485713b4
Ayush Gupta <ayushpguptaapg@gmail.com>
2016-11-21 20:50:12 +0530

    revise tutorial layouts : added flat icons

---
+++ app/src/main/java/in/reweyou/reweyou/customView/CircleOverlayView.java
@@ -0,0 +1,81 @@
+package in.reweyou.reweyou.customView;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import in.reweyou.reweyou.R;
+
+/**
+ * Created by master on 21/11/16.
+ */
+
+public class CircleOverlayView extends LinearLayout {
+    private Bitmap bitmap;
+
+    public CircleOverlayView(Context context) {
+        super(context);
+    }
+
+    public CircleOverlayView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleOverlayView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CircleOverlayView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);
+
+        if (bitmap == null) {
+            createWindowFrame();
+        }
+        canvas.drawBitmap(bitmap, 0, 0, null);
+    }
+
+    protected void createWindowFrame() {
+        bitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas osCanvas = new Canvas(bitmap);
+
+        RectF outerRectangle = new RectF(0, 0, getWidth(), getHeight());
+
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(getResources().getColor(R.color.colorPrimary));
+        paint.setAlpha(255);
+        osCanvas.drawRect(outerRectangle, paint);
+
+        paint.setColor(Color.TRANSPARENT);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT));
+        float centerX = getWidth() / 2;
+        float centerY = getHeight() / 2;
+        float radius = getResources().getDimensionPixelSize(R.dimen.radius);
+        osCanvas.drawCircle(centerX, centerY, radius, paint);
+    }
+
+    @Override
+    public boolean isInEditMode() {
+        return true;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        bitmap = null;
+    }
+}
