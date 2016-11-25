cc29dd2fc5514da0a09880bcb94809fd22f1f675
Varun Pandey <varunn.pandey@gmail.com>
2016-11-26 02:31:48 +0530

    Making custom tabs work in headline text of City and Message Adapter

--- app/src/main/java/in/reweyou/reweyou/adapter/CityAdapter.java
+++ app/src/main/java/in/reweyou/reweyou/adapter/CityAdapter.java
@@ -209,8 +209,7 @@ public class CityAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         final ImageViewHolder viewHolder = (ImageViewHolder) viewHolder2;
         Spannable spannable = new SpannableString(messagelist.get(position).getHeadline());
         Util.linkifyUrl(spannable, new CustomTabsOnClickListener(activity, mCustomTabActivityHelper));
-        viewHolder.headline.setText(spannable);
-        viewHolder.headline.setMovementMethod(LinkMovementMethod.getInstance());
+
 
 
         if (messagelist.get(position).getHead() == null || messagelist.get(position).getHead().isEmpty())
@@ -224,7 +223,8 @@ public class CityAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
             viewHolder.headline.setVisibility(View.GONE);
         else {
             viewHolder.headline.setVisibility(View.VISIBLE);
-            viewHolder.headline.setText(messagelist.get(position).getHeadline());
+            viewHolder.headline.setText(spannable);
+            viewHolder.headline.setMovementMethod(LinkMovementMethod.getInstance());
         }
 
         if (messagelist.get(position).getDate() == null)
