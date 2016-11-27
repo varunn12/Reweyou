06d25f16fe115b3a8e2e6c84eeeffea897e0142d
Ayush Gupta <ayushpguptaapg@gmail.com>
2016-11-27 21:50:10 +0530

    update URL(s)

--- app/src/main/java/in/reweyou/reweyou/utils/Constants.java
+++ app/src/main/java/in/reweyou/reweyou/utils/Constants.java
@@ -16,13 +16,13 @@ public class Constants {
     //public static final String FEED_URL = "https://www.reweyou.in/reweyou/test_list.php";
     public static final String CAMPAIGN_URL = "https://www.reweyou.in/reweyou/campaignfeed.php";
     public static final String READING_URL = "https://www.reweyou.in/reweyou/readingfeed.php";
-    public static final String MY_CITY_URL = "https://www.reweyou.in/reweyou/cityfeed.php";
+    public static final String MY_CITY_URL = "https://www.reweyou.in/reweyou/report_cityfeed.php";
 
     //My Profile Class
     public static final String MY_PROFILE_URL_VERIFY_FOLLOW = "https://www.reweyou.in/reweyou/verify_follow.php";
     public static final String MY_PROFILE_URL_FOLLOW = "https://www.reweyou.in/reweyou/follow_new.php";
     public static final String MY_PROFILE_UPLOAD_URL = "https://www.reweyou.in/reweyou/profile_picture.php";
-    public static final String MY_PROFILE_EDIT_URL = "https://www.reweyou.in/reweyou/profile.php";
+    public static final String MY_PROFILE_EDIT_URL = "https://www.reweyou.in/reweyou/report_profile.php";
 
     public static final String USER_PROFILE_URL_VERIFY_FOLLOW = "https://www.reweyou.in/reweyou/verify_follow.php";
     public static final String USER_PROFILE_URL_FOLLOW = "https://www.reweyou.in/reweyou/read_new.php";
@@ -31,7 +31,6 @@ public class Constants {
     public static final String URL_NOTI_READ_STATUS = "https://www.reweyou.in/reweyou/readstatus.php";
     public static final String MY_NOTIFICATIONS_URL = "https://www.reweyou.in/reweyou/notification.php";
     public static final String MY_TOTAL_NOTIFICATIONS_URL = "https://www.reweyou.in/reweyou/total_notifications.php";
-
 
     public static final int VIEW_TYPE_IMAGE = 5;
     public static final int VIEW_TYPE_VIDEO = 6;
@@ -46,14 +45,15 @@ public class Constants {
     public static final String POST_REPORT_KEY_CATEGORY = "type";
     public static final String POST_REPORT_KEY_TIME = "time";
     public static final String POST_REPORT_KEY_LOCATION = "location";
-    public static final String POST_REPORT_KEY_HEADLINE = "headline";
+    public static final String POST_REPORT_KEY_HEADLINE = "head";
     public static final String POST_REPORT_KEY_NAME = "name";
     public static final String POST_REPORT_KEY_NUMBER = "number";
-    public static final String POST_REPORT_KEY_DESCRIPTION = "description";
+    public static final String POST_REPORT_KEY_DESCRIPTION = "headline";
     public static final String POST_REPORT_KEY_TOKEN = "token";
     public static final String POST_REPORT_KEY_IMAGE = "image";
     public static final String POST_REPORT_KEY_VIDEO = "video";
     public static final String POST_REPORT_KEY_GIF = "gif";
     public static final String POST_REPORT_KEY_REPORT = "report";
 
+    public static final String AUTH_ERROR = "Autherror";
 }
