9a02bf4ed30bc9a13b2170e6aeb18e271605a545
Ayush Gupta <ayushpguptaapg@gmail.com>
2016-11-27 21:48:55 +0530

    add dynamic text on notification action item

--- app/src/main/java/in/reweyou/reweyou/model/NotificationCommentsModel.java
+++ app/src/main/java/in/reweyou/reweyou/model/NotificationCommentsModel.java
@@ -78,4 +78,12 @@ public class NotificationCommentsModel {
     public String getReadstatus() {
         return readstatus;
     }
+
+    public void setReadstatus(String readstatus) {
+        this.readstatus = readstatus;
+    }
+
+    public String getPostid() {
+        return postid;
+    }
 }
--- app/src/main/java/in/reweyou/reweyou/model/NotificationLikesModel.java
+++ app/src/main/java/in/reweyou/reweyou/model/NotificationLikesModel.java
@@ -71,4 +71,12 @@ public class NotificationLikesModel {
     public String getReadstatus() {
         return readstatus;
     }
+
+    public void setReadstatus(String readstatus) {
+        this.readstatus = readstatus;
+    }
+
+    public String getPostid() {
+        return postid;
+    }
 }
