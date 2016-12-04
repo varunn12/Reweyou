194290ea4cf26f959c9c59eb6c52524c091e5538
Ayush Gupta <ayushpguptaapg@gmail.com>
2016-12-04 22:04:46 +0530

    add auth check in edit headline dialog

--- app/src/main/java/in/reweyou/reweyou/utils/Constants.java
+++ app/src/main/java/in/reweyou/reweyou/utils/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
     public static final String REVIEW_URL = "https://www.reweyou.in/reweyou/reviews.php";
     public static final String VIEW_URL = "https://www.reweyou.in/reweyou/postviews.php";
     public static final String SUGGEST_URL = "https://www.reweyou.in/reweyou/suggest.php";
-    public static final String EDIT_URL = "https://www.reweyou.in/reweyou/editheadline.php";
+    public static final String EDIT_URL = "https://www.reweyou.in/reweyou/editheadline_new.php";
 
     public static final String FEED_URL = "https://www.reweyou.in/reweyou/testfeed.php";
     //public static final String FEED_URL = "https://www.reweyou.in/reweyou/test_list.php";
